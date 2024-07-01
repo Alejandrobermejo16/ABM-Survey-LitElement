564eba046092a307e0eee4c767738733c36c7704
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-07-01 11:11:24 +0200

    avances con el paginador

--- src/Components/abm-Screen.js
+++ src/Components/abm-Screen.js
@@ -18,17 +18,87 @@ export class AbmScreen extends LitElement {
     super();
     this.questions = [
       {
-        text: "¿Es la hora de ganar?",
-        description: "Hora de demostrar nuestras habilidades."
+        text: "¿Quieres proponer tus propias preguntas y descripciones?",
+        description: "Al usar el componente, utiliza la prop questions y pasale una array de objetos con prop text para la pregunta y description para la descripcion."
       },
       {
-        text: "¿Se verán correctamente las preguntas?",
-        description: "Asegurémonos de que todo se vea bien."
+        text: "¿Quieres cambiar las preguntas que salen por pagina?",
+        description: "pasale al componente pageSize = 1 o el numero de preguntas que quieres que se vean por pagina" 
       },
       {
-        text: "¿Llegaré a Marte algún día?",
-        description: "El camino del aprendizaje es largo."
-      }
+        text: "¿Quieres cambiar el color de fondo del panel de pregutas?",
+        description: "Pasale al componente colorFrameBackground y el color entre comillas puedes elegir tambien valores exadecimales."
+      },
+      {
+        text: "¿Quieres ver ejemplos?",
+        description: "A partir de la pagina 3."
+      },
+      {
+        text: "¿Quieres cambiar el titulo?",
+        description: "Pasale un string a la prop titleSurvey."
+      },
+      {
+        text: "¿Tienes nuevas sugerencias?",
+        description: "Envia un correo a abmprojects9@gmail.com y tendremos en cuenta tus peticiones para mostrar nuevos cambios."
+      },
+      {
+        text: "¿Quieres incluir imagenes en tu pregunta?",
+        description: "Pasa a tu array de preguntas la propiedad img, con la la ruta de la imagen que quieres que se vea y si quieres ponerle alt , pasale alt."
+      },
+      {
+        text: "¿Quieres poner otro tipo de input?",
+        description: "Pasa a tu array de preguntas la propiedad type seguido de select/radio/checkbox, y debajo la prop options que sera un array con  { value: yes, label: Yes }, ejemplos en la siguiente pagina"
+      },
+      {
+        text: "¿Quieres poner otro tipo de input?",
+        description: "Pasa a tu array de preguntas la propiedad type seguido de select/radio/checkbox, y debajo la prop options que sera un array con  { value: yes, label: Yes }, ejemplos en la siguiente pagina",
+        type: "radio",
+        options: [
+          {value: "yes", label: "yes"},
+          {value: "no", label: "no"}
+        ]
+      },
+      {
+        text: "¿Quieres poner otro Ejemplo?",
+        description: "Pasa a tu array de preguntas la propiedad type seguido de select/radio/checkbox, y debajo la prop options que sera un array con  { value: yes, label: Yes }, ejemplos en la siguiente pagina",
+        type: "select",
+        options: [
+          {value: "yes", label: "yes"},
+          {value: "no", label: "no"}
+        ]
+      },
+      {
+        text: "¿Quieres poner otro Ejemplo?",
+        description: "Pasa a tu array de preguntas la propiedad type seguido de select/radio/checkbox, y debajo la prop options que sera un array con  { value: yes, label: Yes }, ejemplos en la siguiente pagina",
+        type: "checkbox",
+        options: [
+          {value: "yes", label: "yes"},
+          {value: "no", label: "no"}
+        ]
+      },
+      {
+        text: "¿Quieres poner un solo elemento en la pagina que estas",
+        description: "Pasa a tu array de preguntas la propiedad unique: true, con el numero de elementos que quieres que aparezcan en esa pagina concreta , por defecto una",
+        unique: true,
+      },
+      {
+        text: "¿Quieres poner otro Ejemplo?",
+        description: "Pasa a tu array de preguntas la propiedad type seguido de select/radio/checkbox, y debajo la prop options que sera un array con  { value: yes, label: Yes }, ejemplos en la siguiente pagina",
+        type: "checkbox",
+        options: [
+          {value: "yes", label: "yes"},
+          {value: "no", label: "no"}
+        ]
+      },
+      {
+        text: "¿Quieres poner otro Ejemplo?",
+        description: "Pasa a tu array de preguntas la propiedad type seguido de select/radio/checkbox, y debajo la prop options que sera un array con  { value: yes, label: Yes }, ejemplos en la siguiente pagina",
+        type: "checkbox",
+        options: [
+          {value: "yes", label: "yes"},
+          {value: "no", label: "no"}
+        ]
+      },
     ].map((question, index) => ({
       id: index + 1,
       ...question
--- src/Components/abm-Survey.js
+++ src/Components/abm-Survey.js
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from "lit-element";
+import { LitElement, html, css } from 'lit-element';
 
 export class AbmSurvey extends LitElement {
   static styles = css`
@@ -8,42 +8,47 @@ export class AbmSurvey extends LitElement {
       width: 900px;
     }
 
-    h1{
-    color: white;
-    display: flex;
-    justify-content: center;
+    h1 {
+      color: white;
+      display: flex;
+      justify-content: center;
     }
 
     .survey-container {
       display: flex;
       flex-direction: column;
-      justify-content: space-between; /* Alinea los elementos de manera uniforme en el espacio vertical */
+      justify-content: space-between;
       height: 100%;
       width: 100%;
       border: 3px solid blue;
       border-radius: 10%;
       background-color: var(--survey-background-color, #3d4c52);
-      padding: 20px; /* Añadido para separar las preguntas */
+      padding: 20px;
+      overflow-y: auto;
+    }
+
+    ::-webkit-scrollbar {
+      display: none;
     }
 
     .question-item {
       display: flex;
-      flex-direction: column; /* Alinea los elementos verticalmente */
-      align-items: flex-start; /* Alinea todos los elementos al inicio (a la izquierda) */
-      margin-bottom: 20px; /* Espacio entre las preguntas */
+      flex-direction: column;
+      align-items: flex-start;
+      margin-bottom: 20px;
     }
 
     .question-content {
       display: flex;
-      align-items: center; /* Alinea el checkbox y el texto de la pregunta en la misma línea */
-      gap: 10px; /* Espacio entre el checkbox y el texto de la pregunta */
+      align-items: center;
+      gap: 10px;
     }
 
     .checkbox {
-      font-size: 30px; /* Ajusta el tamaño de la fuente del checkbox */
-      width: 20px; /* Ancho del checkbox */
-      height: 20px; /* Alto del checkbox */
-      margin-left: 10px; /* Espacio a la izquierda del checkbox */
+      font-size: 30px;
+      width: 20px;
+      height: 20px;
+      margin-left: 10px;
     }
 
     .questionsParraf {
@@ -52,81 +57,213 @@ export class AbmSurvey extends LitElement {
     }
 
     .descriptionParraf {
-      font-size: 20px; /* Tamaño de fuente menor para la descripción */
-      color: #ccc; /* Color gris claro para la descripción */
-      margin-top: 5px; /* Espacio entre la pregunta y la descripción */
-      margin-left: 30px; /* Espacio a la izquierda de la descripción para alinearla con el texto de la pregunta */
+      font-size: 20px;
+      color: #ccc;
+      margin-top: 5px;
+      margin-left: 30px;
     }
 
     .footerSurvey {
       display: flex;
       justify-content: center;
-      margin-top: auto; /* Empuja el pie de página hacia la parte inferior */
-      padding-top: 20px; /* Espacio adicional arriba del pie de página */
+      margin-top: auto;
+      padding-top: 20px;
       border-top: 1px solid black;
       border-radius: 10%;
-    
-      button {
-        font-size: 40px;
-        border-radius: 10%;
-        margin: 10px;
-      }
+    }
+
+    .footerSurvey button {
+      font-size: 40px;
+      border-radius: 10%;
+      margin: 10px;
+    }
+
+    .page {
+      color: white;
+      font-size: 20px;
     }
   `;
 
   static get properties() {
     return {
-      colorFrameBackground: { type: String }, // Frame background color
-      questions: { type: Array }, // Array of questions for the survey
-      checked: { type: Boolean }, // value of input check
+      colorFrameBackground: { type: String },
+      questions: { type: Array },
+      checked: { type: Boolean },
       titleSurvey: { type: String },
-      description: { type: String }
+      description: { type: String },
+      page: { type: Number },
+      pageSize: { type: Number },
+      img: { type: String },
+      alt: { type: String },
     };
   }
 
   constructor() {
     super();
-    this.colorFrameBackground = "#3D4C52"; 
+    this.colorFrameBackground = '#3D4C52';
     this.questions = [];
     this.checked = false;
-    this.titleSurvey = "Título de la encuesta";
-    this.description = "";
+    this.titleSurvey = 'Título de la encuesta';
+    this.description = '';
+    this.page = 0; //start page
+    this.pageSize = 4; //number of questions per page
+    this.img = '';
+    this.alt = '';
   }
 
   toggleInput(id) {
     this.checked = !this.checked;
-    console.log("El estado del checkbox ha cambiado", this.checked);
-    console.log("El id del checkbox es", id);
+    console.log('El estado del checkbox ha cambiado', this.checked);
+    console.log('El id del checkbox es', id);
+  }
+
+  pagging(e) {
+    const nameButton = e.target.classList.contains('next') ? 'next' : 'previous';
+    const totalQuestions = this.questions.length;
+
+    // Encuentra la pregunta `unique` en la página actual.
+    const uniqueQuestionIndex = this.questions.findIndex(
+      (q, index) => q.unique && Math.floor(index / this.pageSize) === this.page
+    );
+
+    if (nameButton === 'next') {
+      // Si hay una pregunta `unique` en la página actual, muestra solo esa pregunta en la siguiente página.
+      if (uniqueQuestionIndex !== -1) {
+        this.page += 1;
+      } else {
+        // Avanza a la siguiente página.
+        if (this.page < Math.ceil(totalQuestions / this.pageSize) - 1) {
+          this.page += 1;
+        } else {
+          // Si estamos en la última página de preguntas normales, avanzar a la página de preguntas `unique` si existe
+          const nextUniquePage = this.questions.findIndex(
+            (q, index) => q.unique && Math.floor(index / this.pageSize) > this.page
+          );
+          if (nextUniquePage !== -1) {
+            this.page = Math.floor(nextUniquePage / this.pageSize);
+          }
+        }
+      }
+    } else if (nameButton === 'previous') {
+      // Retroceder a la página anterior si no estamos en la primera página
+      if (this.page > 0) {
+        // Si estamos en una página de `unique`, vamos a la página anterior en la paginación normal
+        const prevUniqueQuestionIndex = this.questions.findIndex(
+          (q, index) => q.unique && Math.floor(index / this.pageSize) === this.page - 1
+        );
+        if (prevUniqueQuestionIndex !== -1) {
+          this.page -= 1;
+        } else {
+          this.page -= 1;
+        }
+      }
+    }
+    this.requestUpdate();
   }
 
   render() {
+    // Encuentra la pregunta `unique` en la página actual.
+    const uniqueQuestionIndex = this.questions.findIndex(
+      (q, index) => q.unique && Math.floor(index / this.pageSize) === this.page
+    );
+
+    // Si hay una pregunta `unique` en la página actual, solo muéstrala en esa página.
+    const visibleQuestions = uniqueQuestionIndex !== -1
+      ? [this.questions[uniqueQuestionIndex]]
+      : this.questions.slice(
+          this.page * this.pageSize,
+          (this.page + 1) * this.pageSize
+        );
+
     return html`
-      <div class="survey-container">
+      <div class="survey-container" style="background-color: ${this.colorFrameBackground};">
+        ${this.img ? html`<img src="${this.img}" alt="${this.alt}" class="survey-image"/>` : ''}
         <h1>${this.titleSurvey}</h1>
-        ${this.questions.map(
+        ${visibleQuestions.map(
           (question) => html`
             <div class="question-item">
               <div class="question-content">
-                <input
-                  type="checkbox"
-                  id="${question.id}"
-                  name="scales"
-                  class="checkbox"
-                  @click="${() => this.toggleInput(question.id)}"
-                />
+                ${question.type === 'checkbox'
+                  ? question.options.map(
+                      option => html`
+                        <input
+                          type="checkbox"
+                          id="${option.value}"
+                          name="question-${question.id}"
+                          class="checkbox"
+                          @click="${() => this.toggleInput(question.id)}"
+                        />
+                        <label for="${option.value}">${option.label}</label>
+                      `
+                    )
+                  : question.type === 'radio'
+                  ? question.options.map(
+                      option => html`
+                        <input
+                          type="radio"
+                          id="${option.value}"
+                          name="question-${question.id}"
+                          class="radio"
+                          @click="${() => this.toggleInput(question.id)}"
+                        />
+                        <label for="${option.value}">${option.label}</label>
+                      `
+                    )
+                  : question.type === 'select'
+                  ? html`
+                    <select
+                      id="${question.id}"
+                      name="question-${question.id}"
+                      class="select"
+                    >
+                      ${question.options.map(
+                        option => html`<option value="${option.value}">${option.label}</option>`
+                      )}
+                    </select>
+                  `
+                  : question.type === 'text'
+                  ? html`
+                    <input
+                      type="text"
+                      id="${question.id}"
+                      name="question-${question.id}"
+                      class="text"
+                    />
+                  `
+                  : ''
+                }
                 <p class="questionsParraf">${question.text}</p>
               </div>
-              ${question.description ? html`<p class="descriptionParraf">${question.description}</p>` : ''}
+              ${question.description
+                ? html`<p class="descriptionParraf">${question.description}</p>`
+                : ""}
             </div>
           `
         )}
         <div class="footerSurvey">
-          <button>Anterior</button>
-          <button>Siguiente</button>
+          ${this.page > 0
+            ? html`<button class="previous" @click="${(e) => this.pagging(e)}">
+                Anterior
+              </button>`
+            : ''
+          }
+          <p class="page">${this.page + 1}</p>
+          ${((!this.questions[(this.page + 1) * this.pageSize]?.unique && this.page < Math.ceil(this.questions.length / this.pageSize) - 1) ||
+              this.questions.some(q => q.unique) ||
+              (this.questions[this.page * this.pageSize]?.unique && !this.questions[(this.page + 1) * this.pageSize])
+            ) && 
+          ((this.page < Math.ceil(this.questions.length / this.pageSize) - 1) || this.questions.some(q => q.unique) ||
+          (this.questions[this.page * this.pageSize]?.unique && !this.questions[(this.page + 1) * this.pageSize])
+            )
+            ? html`<button class="next" @click="${(e) => this.pagging(e)}">
+                Siguiente
+              </button>`
+            : ''
+          }
         </div>
       </div>
     `;
   }
 }
 
-customElements.define("abm-survey", AbmSurvey);
+customElements.define('abm-survey', AbmSurvey);
